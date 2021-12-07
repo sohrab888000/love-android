0fbada74c2c31e1f38130284d8543a051171364a
sohrab888000 <50212481+sohrab888000@users.noreply.github.com>
2021-12-07 13:07:09 -0800

    Update MainActivity.java

--- love/src/main/java/org/love2d/android/MainActivity.java
+++ love/src/main/java/org/love2d/android/MainActivity.java
@@ -865,12 +865,17 @@ sFourEthree.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
 	
 //###init scoore and stars :
 //-------season one
+int stars = 0;	
 Button sOneEoneStars = (Button) findViewById(R.id.sOneEoneStars);
+stars = setStarsAndScores("s1e1_save.txt",sOneEoneStars);	
+if (stars == 1)
+sOneEoneStars.setCompoundDrawablesWithIntrinsicBounds(null, R.drawable.star_one , null, null);	
+	
 Button sOneEtwoStars = (Button) findViewById(R.id.sOneEtwoStars);	
 Button sOneEthreeStars = (Button) findViewById(R.id.sOneEthreeStars);		
-setStarsAndScores("s1e1_save.txt",sOneEoneStars);	
 setStarsAndScores("s1e2_save.txt",sOneEtwoStars);	
 setStarsAndScores("s1e3_save.txt",sOneEthreeStars);	
+	
 /*
 //-------season two
 Button sTwoEone = (Button) findViewById(R.id.sTwoEone);	
@@ -965,7 +970,7 @@ public static void ImageViewAnimatedChange(final Context c,final ImageView v ,fi
 //###################################################################	functions need for Animations!
 	
 //###################################################################functions for helping showing scoores 
-public void setStarsAndScores(String FileName,Button b){	
+public int setStarsAndScores(String FileName,Button b){	
 String storagePath  = "";
 int stars = 0;	
 double time = 0;
@@ -999,18 +1004,11 @@ s = time - ( (m * 60) + (h * 3600) );
 }		
 String	texttt = Double.toString(h) + ":" + Double.toString(m) + ":" + Double.toString(s); 
 b.setText(texttt);
-	
-if(stars == 1)	
-b.setCompoundDrawablesWithIntrinsicBounds(null, R.drawable.star_one, null, null);		
-else if(stars == 2)
-b.setCompoundDrawablesWithIntrinsicBounds(null, R.drawable.star_two, null, null);		
-else if(stars == 3)
-b.setCompoundDrawablesWithIntrinsicBounds(null, R.drawable.star_three, null, null);		
 }else{//file does'nt exist
 String	texttt = "00" + ":" + "00" + ":" + "00"; 
 b.setText(texttt);
-b.setCompoundDrawablesWithIntrinsicBounds(null, R.drawable.star_zero, null, null);			
-}	
+}
+return stars;	
 }
 //###################################################################functions for helping showing scoores 
 	
